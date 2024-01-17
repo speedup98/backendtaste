c4e514db66563ccf462f03d28effe17ac2d90c9f
speedup98 <speedup98@gmail.com>
2024-01-17 20:19:47 +0900

    폴더 구조 최적화

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ app.set("views", '../views');
 app.set("view engine", 'ejs');
 
 //미들웨어 등록
-const home = require('./routes/home');
+const home = require('./src/routes/home');
 app.use("/", home)
 
 module.exports = app;
