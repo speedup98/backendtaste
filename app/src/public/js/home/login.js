feb88ef0a648d2311f558d6d3a1248db0481f999
speedup98 <speedup98@gmail.com>
2024-01-31 19:58:55 +0900

    20240131

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,10 +22,11 @@ function login() {
         res.json()
     )
     .then((res) => {
-        // alert(res.msg);
-        // if (res.success) {
-        //     location.href="/";
-        // }
+        if (res.success) {
+            location.href="/";
+        } else {
+            alert(res.msg);
+        }
     })
     .catch((err) =>{
         console.error(new Error("로그인 중 에러 발생"));
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -18,16 +18,15 @@ const process = {
         psword = req.body.psword
         //인자값으로 들어올때 작은따옴표로 처리해야 한다.
         const users = UserStorage.getUsers('id', 'psword');
-
         const response = {};
-        // if (users.id.includes(id)) {
-        //     const idx = users.id.indexOf(id);
-        //     if (users.psword[idx] === psword) {
-        //         response.success = true;
-        //         response.msg = "로그인에 성공 하셨습니다.";
-        //         return res.json (response);
-        //     }
-        // }
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                response.success = true;
+                response.msg = "로그인에 성공 하셨습니다.";
+                return res.json (response);
+            }
+        }
         response.success = false;
         response.msg = "로그인에 실패 하셨습니다.";
         return res.json(response);
